14b8f1ad5725da01e507f8dc13c883c9e2be2422
Merlin Barzilai <merlin.barzilai@gmail.com>
2026-04-24 08:49:24 +0000

    solved: using a custom compare function and refactoring the call site

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-fn sort_usernames<T: AsRef<str>>(usernames: &mut Vec<T>) {
-    todo!();
+use std::cmp::Ordering;
+
+fn sort_usernames<T: AsRef<str>>(mut usernames: Vec<T>) {
+    todo!()
+}
+
+fn case_insensitive_sort<T: AsRef<str>>(a: &T, b: &T) -> Ordering {
+    String::cmp(&a.as_ref().to_lowercase(), &b.as_ref().to_lowercase())
 }
 
 fn main() {
     let mut users = vec!["Todd", "Amy", "mike99", "Jennifer", "alison"];
 
     println!("unsorted: {:?}", &users);
-    sort_usernames(&mut users);
+    users.sort_by(case_insensitive_sort);
     println!("sorted:   {:?}", &users);
 }
 
@@ -14,7 +20,7 @@ fn main() {
 fn five_users() {
     let mut users = vec!["Todd", "Amy", "mike99", "Jennifer", "alison"];
     let sorted = vec!["alison", "Amy", "Jennifer", "mike99", "Todd"];
-    sort_usernames(&mut users);
+    users.sort_by(case_insensitive_sort);
 
     assert_eq!(users, sorted);
 }
