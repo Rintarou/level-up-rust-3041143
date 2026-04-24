df41347cd3d67dcea175feb2203bb863439c7d65
Merlin Barzilai <merlin.barzilai@gmail.com>
2026-04-24 13:58:37 +0000

    solved: wrapping values in the Temperature enum - replacing the split between scale and value

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,77 @@
 #![allow(unused)]
 
-enum Scale {
-    Celsius,
-    Fahrenheit,
-}
+use std::convert::Into;
 
-struct Temperature {
-    degrees: f32,
-    scale: Scale,
+use Temperature::*;
+
+enum Temperature {
+    Celsius(f32),
+    Fahrenheit(f32),
 }
 
 impl Temperature {
-    fn new(degrees: f32) -> Self {
-        Temperature {
-            degrees,
-            scale: Scale::Celsius,
+    fn to_celsius(&self) -> Temperature {
+        match self {
+            Celsius(celsius) => Celsius(*celsius),
+            Fahrenheit(fahrenheit) => Celsius(5. * (*fahrenheit - 32.) / 9.),
         }
     }
 
-    fn to_celsius(&self) -> f32 {
-        todo!();
+    fn to_fahrenheit(&self) -> Temperature {
+        match self {
+            Celsius(celsius) => Fahrenheit(32. + (9. * *celsius / 5.)),
+            Fahrenheit(fahrenheit) => Fahrenheit(*fahrenheit),
+        }
     }
 
-    fn to_fahrenheit(&self) -> f32 {
-        todo!();
+    fn to_f32(&self) -> f32 {
+        match self {
+            Celsius(celsius) => *celsius,
+            Fahrenheit(fahrenheit) => *fahrenheit,
+        }
     }
 }
 
+/*
+impl Into<f32> for Temperature {
+    fn into(self) -> f32 {
+        match self {
+            Celsius(f) => f.clone(),
+            Fahrenheit(f) => f.clone(),
+        }
+    }
+}
+ */
+
 fn main() {
-    let temp = Temperature::new(20.0);
+    let temp = Celsius(0.0);
 
     println!("fun fact: 20°C is an integer in celsius and fahrenheit");
-    println!("          {:.1}°C = {:.1}°F", temp.to_celsius(), temp.to_fahrenheit());
+    println!(
+        "          {:.1}°C = {:.1}°F",
+        temp.to_celsius().to_f32(),
+        temp.to_fahrenheit().to_f32()
+    );
 }
 
 #[test]
 fn one_degree() {
-    let cold = Temperature::new(1.0);
-    assert!((cold.to_fahrenheit() - 33.8) < 0.01);
-    assert!((cold.to_fahrenheit() - 33.8) >= 0.0);
+    let cold = Celsius(1.0);
+    assert!((cold.to_fahrenheit().to_f32() - 33.8) < 0.01);
+    assert!((cold.to_fahrenheit().to_f32() - 33.8) >= 0.0);
 }
 
 #[test]
 fn boiling() {
-    let hot = Temperature::new(100.0);
-    assert!((hot.to_fahrenheit() - 212.0) < 0.01);
-    assert!((hot.to_fahrenheit() - 212.0) >= 0.0);
+    let hot = Celsius(100.0);
+    assert!((hot.to_fahrenheit().to_f32() - 212.0) < 0.01);
+    assert!((hot.to_fahrenheit().to_f32() - 212.0) >= 0.0);
 }
 
 #[test]
 fn freezing() {
-    let freezing = Temperature {
-        degrees: Temperature::new(0.0).to_fahrenheit(),
-        scale: Scale::Fahrenheit,
-    };
+    let freezing = Celsius(0.0).to_fahrenheit();
 
-    assert!(freezing.to_celsius() < 0.001);
-    assert!(freezing.to_celsius() > -0.01);
+    assert!(freezing.to_celsius().to_f32() < 0.001);
+    assert!(freezing.to_celsius().to_f32() > -0.01);
 }
-
